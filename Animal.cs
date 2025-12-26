b78e958c15f91a7aba0145150acfde3aa2a585f9
majid <majidvp501@gmail.com>
2025-12-26 19:31:24 +0530

    Removed All Changes

--- Animal.cs
+++ Animal.cs
@@ -28,8 +28,6 @@ namespace OOPsBasicTask
         public  void meow()
         {
             Console.WriteLine("Meowwww..");
-            Console.WriteLine("Hey i am Kitti");
-            Console.WriteLine("Hey there");
         }
     }
 }
