ff2f3a0d721d057ec1a6a058fa37503b40821285
majid <majidvp501@gmail.com>
2025-12-26 18:55:27 +0530

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsBasicTask
+{
+    internal class Animal
+    {
+        public string name;
+        public decimal age;
+
+        public void Speak()
+        {
+            Console.WriteLine($"{name}-{age} yeaars Old");
+        }
+    }
+
+
+    internal class Dog : Animal
+    {
+        public string breed;
+    }
+
+    internal class Cat : Animal
+    {
+        public  void meow()
+        {
+            Console.WriteLine("Meowwww..");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using OOPsBasicTask;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Dog myDog = new Dog();
+        myDog.name = "Jimmii";
+        myDog.age = 2;
+        myDog.breed = "fris";
+        myDog.Speak();
+
+
+        Cat myCat = new Cat();
+        myCat.name = "kiyiyi";
+        myCat.age = 1;
+        myCat.meow();
+        myCat.Speak();
+
+
+
+
+    }
+}
